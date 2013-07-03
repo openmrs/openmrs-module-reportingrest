15ae56bf81a38d0fa403430d44d1e564da9e6258
unknown <Sashrika@Sashrika-PC.(none)>
2013-07-03 21:42:22 +0530

    all methods and attributes of the BaseDefinitionResource class has added to the ReportDefinition class as the Design page of Roger

--- omod/src/main/java/org/openmrs/module/reportingrest/web/resource/ReportDefinitionResource.java
+++ omod/src/main/java/org/openmrs/module/reportingrest/web/resource/ReportDefinitionResource.java
@@ -23,18 +23,83 @@ import org.openmrs.module.webservices.rest.web.representation.Representation;
 import org.openmrs.module.webservices.rest.web.resource.impl.BaseDelegatingResource;
 import org.openmrs.module.webservices.rest.web.resource.impl.DelegatingResourceDescription;
 
+
+import java.util.List;
+
+import org.openmrs.module.reporting.definition.DefinitionContext;
+import org.openmrs.module.reporting.evaluation.Definition;
+import org.openmrs.module.webservices.rest.web.RequestContext;
+import org.openmrs.module.webservices.rest.web.RestConstants;
+import org.openmrs.module.webservices.rest.web.annotation.Resource;
+import org.openmrs.module.webservices.rest.web.representation.DefaultRepresentation;
+import org.openmrs.module.webservices.rest.web.representation.FullRepresentation;
+import org.openmrs.module.webservices.rest.web.representation.Representation;
+import org.openmrs.module.webservices.rest.web.resource.api.PageableResult;
+import org.openmrs.module.webservices.rest.web.resource.api.Searchable;
+import org.openmrs.module.webservices.rest.web.resource.impl.BaseDelegatingResource;
+import org.openmrs.module.webservices.rest.web.resource.impl.DelegatingResourceDescription;
+import org.openmrs.module.webservices.rest.web.resource.impl.MetadataDelegatingCrudResource;
+import org.openmrs.module.webservices.rest.web.resource.impl.NeedsPaging;
+import org.openmrs.module.webservices.rest.web.response.ResponseException;
+
 /**
  * {@link Resource} for {@link ReportDefinition}s, supporting standard CRUD operations
  */
 @Resource(name = RestConstants.VERSION_1 + ReportingRestController.REPORTING_REST_NAMESPACE + "/reportDefinition",
         supportedClass = ReportDefinition.class, supportedOpenmrsVersions = {"1.8.*", "1.9.*"})
-public class ReportDefinitionResource extends BaseDefinitionResource<ReportDefinition> {
+public class ReportDefinitionResource extends MetadataDelegatingCrudResource<ReportDefinition> implements Searchable {
 	
 	/**
 	 * @return the definition type that this resource wraps
 	 */
 	public Class<ReportDefinition> getDefinitionType() {
 		return ReportDefinition.class;
+	}
+	
+	/**
+	 * @see BaseDelegatingResource#newDelegate()
+	 */
+	@Override
+    public ReportDefinition newDelegate() {
+		try {
+			return getDefinitionType().newInstance();
+		}
+		catch (Exception e) {
+			throw new RuntimeException("Unable to create new " + getDefinitionType());
+		}
+	}
+	
+	/**
+	 * @see BaseDelegatingResource#getByUniqueId(String)
+	 */
+	@Override
+	public ReportDefinition getByUniqueId(String uuid) {
+		return DefinitionContext.getDefinitionByUuid(getDefinitionType(), uuid);
+	}
+	
+	/**
+	 * @see BaseDelegatingResource#save(Object)
+	 */
+	@Override
+	public ReportDefinition save(ReportDefinition definition) {
+		return DefinitionContext.saveDefinition(definition);
+	}
+
+	/**
+	 * @see BaseDelegatingResource#delete(Object, String, RequestContext)
+	 */
+	@Override
+	public void delete(ReportDefinition definition, String reason, RequestContext context) throws ResponseException {
+		definition.setRetireReason(reason);
+		DefinitionContext.retireDefinition(definition);
+	}
+	
+	/**
+	 * @see BaseDelegatingResource#purge(Object, RequestContext)
+	 */
+	@Override
+	public void purge(ReportDefinition definition, RequestContext context) throws ResponseException {
+		DefinitionContext.purgeDefinition(getDefinitionType(), definition.getUuid());
 	}
 
 	/**
@@ -43,6 +108,62 @@ public class ReportDefinitionResource extends BaseDefinitionResource<ReportDefin
 	@Override
 	public DelegatingResourceDescription getRepresentationDescription(Representation rep) {
 		// TODO, add more properties here, eg. DSDs and BaseCohort
-		return super.getRepresentationDescription(rep); 
+        DelegatingResourceDescription description = null;
+		
+		if (rep instanceof DefaultRepresentation) {
+			description = new DelegatingResourceDescription();
+			description.addProperty("uuid");
+			description.addProperty("name");
+			description.addProperty("description");
+			description.addProperty("parameters");
+			description.addSelfLink();
+			description.addLink("full", ".?v=" + RestConstants.REPRESENTATION_FULL);
+		} 
+		else if (rep instanceof FullRepresentation) {
+			description = new DelegatingResourceDescription();
+			description.addProperty("uuid");
+			description.addProperty("name");
+			description.addProperty("description");
+			description.addProperty("parameters");
+			description.addSelfLink();
+		}
+		return description;
+	}
+	
+	/**
+	 * @see org.openmrs.module.webservices.rest.web.resource.impl.DelegatingCrudResource#doSearch(org.openmrs.module.webservices.rest.web.RequestContext)
+	 */
+	@Override
+	protected PageableResult doSearch(RequestContext context) {
+        String query = context.getParameter("q");
+		List<ReportDefinition> results = DefinitionContext.getDefinitionService(getDefinitionType()).getDefinitions(query, false);
+		return new NeedsPaging<ReportDefinition>(results, context);
+	}
+	
+	/**
+	 * @see org.openmrs.module.webservices.rest.web.resource.impl.DelegatingCrudResource#doGetAll(org.openmrs.module.webservices.rest.web.RequestContext)
+	 */
+	public PageableResult doGetAll(RequestContext context) throws ResponseException {
+		return new NeedsPaging<ReportDefinition>(DefinitionContext.getDefinitionService(getDefinitionType()).getAllDefinitions(false), context);
+	}
+	
+	/**
+	 * @param delegate
+	 * @return the URI for the given delegate object
+	 */
+	@SuppressWarnings("unchecked")
+	@Override
+	public String getUri(Object delegate) {
+		if (delegate == null)
+			return "";
+		
+		Resource res = getClass().getAnnotation(Resource.class);
+		if (res != null) {
+			String url = RestConstants.URI_PREFIX + res.name() + "/" + getUniqueId((ReportDefinition) delegate);
+			url = url.replace("/rest/", "/reporting/"); // hacky :-(
+			return url;
+		}
+		throw new RuntimeException(getClass() + " needs a @Resource or @SubResource annotation");
+		
 	}
 }
