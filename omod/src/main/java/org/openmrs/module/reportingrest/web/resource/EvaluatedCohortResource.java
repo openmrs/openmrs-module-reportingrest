9ce82a83b1ba6ffae890582cf653f89ce3bb0986
unknown <Sashrika@Sashrika-PC.(none)>
2013-09-21 19:19:03 +0530

    Adding ref and full representation to the evaluated cohort resource

--- omod/src/main/java/org/openmrs/module/reportingrest/web/resource/EvaluatedCohortResource.java
+++ omod/src/main/java/org/openmrs/module/reportingrest/web/resource/EvaluatedCohortResource.java
@@ -31,6 +31,8 @@ import org.openmrs.module.webservices.rest.web.RestConstants;
 import org.openmrs.module.webservices.rest.web.annotation.PropertyGetter;
 import org.openmrs.module.webservices.rest.web.annotation.Resource;
 import org.openmrs.module.webservices.rest.web.representation.DefaultRepresentation;
+import org.openmrs.module.webservices.rest.web.representation.FullRepresentation;
+import org.openmrs.module.webservices.rest.web.representation.RefRepresentation;
 import org.openmrs.module.webservices.rest.web.representation.Representation;
 import org.openmrs.module.webservices.rest.web.resource.impl.DelegatingResourceDescription;
 import org.openmrs.module.webservices.rest.web.response.ConversionException;
@@ -111,8 +113,18 @@ public class EvaluatedCohortResource extends EvaluatedResource<EvaluatedCohort> 
 			description.addProperty("definition");
 			description.addProperty("members", Representation.REF); // @PropertyGetter method below
 			description.addSelfLink();
-		}
-
+		}else if(rep instanceof FullRepresentation){
+            description = new DelegatingResourceDescription();
+            description.addProperty("uuid");
+            description.addProperty("definition");
+            description.addProperty("members", Representation.REF); // @PropertyGetter method below
+            description.addSelfLink();
+        }else if(rep instanceof RefRepresentation){
+            description = new DelegatingResourceDescription();
+            description.addProperty("uuid");
+            description.addProperty("definition");
+            description.addSelfLink();
+        }
 		return description;
 	}
 
