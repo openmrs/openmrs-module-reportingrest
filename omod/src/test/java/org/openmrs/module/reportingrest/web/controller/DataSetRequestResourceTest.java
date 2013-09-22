4fee2acdd8fc9d6da05a482d5d5820cd7f24f12b
unknown <Sashrika@Sashrika-PC.(none)>
2013-09-22 17:03:01 +0530

    Adding testcase for Dataset request resource

---
+++ omod/src/test/java/org/openmrs/module/reportingrest/web/controller/DataSetRequestResourceTest.java
@@ -0,0 +1,86 @@
+package org.openmrs.module.reportingrest.web.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.openmrs.module.reporting.cohort.definition.CohortDefinition;
+import org.openmrs.module.reporting.dataset.definition.CohortCrossTabDataSetDefinition;
+import org.openmrs.module.reporting.dataset.definition.DataSetDefinition;
+import org.openmrs.module.reporting.evaluation.parameter.Mapped;
+import org.openmrs.module.reporting.report.ReportRequest;
+import org.openmrs.module.reporting.report.ReportRequest.Priority;
+import org.openmrs.module.reporting.report.ReportRequest.Status;
+import org.openmrs.module.reporting.report.definition.ReportDefinition;
+import org.openmrs.module.reporting.report.renderer.RenderingMode;
+import org.openmrs.module.reporting.web.renderers.DefaultWebRenderer;
+import org.openmrs.module.reportingrest.web.resource.ReportRequestResource;
+
+public class DataSetRequestResourceTest extends BaseDelegatingResourceTest<ReportRequestResource,ReportRequest> {
+
+    @Override
+    public ReportRequest newObject() {
+        ReportRequest reportRequest=new ReportRequest();
+        CohortCrossTabDataSetDefinition crosstab=new CohortCrossTabDataSetDefinition();
+        crosstab.setName("cohortWrappingDataset");
+        Map<String, Mapped<? extends CohortDefinition>> mapi=new HashMap<String, Mapped<? extends CohortDefinition>>();
+        crosstab.setColumns(mapi);
+        Mapped<DataSetDefinition> mappedData=new Mapped<DataSetDefinition>(); mappedData.setParameterizable(crosstab);
+        Map<String, Mapped<? extends DataSetDefinition>> definition=new HashMap<String, Mapped<? extends DataSetDefinition>>();
+        definition.put("cohort key created by program",  mappedData);
+        ReportDefinition reportdef=new ReportDefinition();
+        reportdef.setDataSetDefinitions(definition);
+        reportdef.setName("Cohortwrapper");
+        reportdef.setDescription("Helper for create cohort request");
+        Mapped<ReportDefinition> mappedRep=new Mapped<ReportDefinition>();
+        mappedRep.setParameterizable(reportdef);
+        reportRequest.setReportDefinition(mappedRep);
+        RenderingMode mode=new RenderingMode();
+        DefaultWebRenderer render=new DefaultWebRenderer();
+        mode.setRenderer(render);
+        reportRequest.setRenderingMode(mode);
+        reportRequest.setPriority(Priority.NORMAL);
+        reportRequest.setStatus(Status.REQUESTED);
+        reportRequest.setUuid("c11f5354-9567-4cc5-b3ef-163e28873926");
+        return reportRequest;
+    }
+
+    @Override
+    public void validateRefRepresentation() throws Exception {
+        super.validateRefRepresentation();
+        assertPropEquals("status",newObject().getStatus());
+
+    }
+
+    @Override
+    public void validateDefaultRepresentation() throws Exception {
+        super.validateDefaultRepresentation();
+        assertPropEquals("priority",newObject().getPriority());
+        assertPropPresent("requestedBy");
+        assertPropPresent("requestDate");
+        assertPropEquals("status",newObject().getStatus());
+        assertPropEquals("evaluateStartDatetime",newObject().getEvaluateStartDatetime());
+        assertPropEquals("evaluateCompleteDatetime",newObject().getEvaluateCompleteDatetime());
+    }
+
+    @Override
+    public void validateFullRepresentation() throws Exception {
+        super.validateFullRepresentation();
+        assertPropEquals("priority",newObject().getPriority());
+        assertPropPresent("requestedBy");
+        assertPropPresent("requestDate");
+        assertPropEquals("status",newObject().getStatus());
+        assertPropEquals("evaluateStartDatetime",newObject().getEvaluateStartDatetime());
+        assertPropEquals("evaluateCompleteDatetime",newObject().getEvaluateCompleteDatetime());
+        assertPropEquals("schedule",newObject().getSchedule());
+
+    }
+
+    @Override
+    public String getDisplayProperty() {
+        return "cohortWrappingDataset";
+    }
+
+    @Override
+    public String getUuidProperty() {
+        return getObject().getUuid();
+    }
+}
