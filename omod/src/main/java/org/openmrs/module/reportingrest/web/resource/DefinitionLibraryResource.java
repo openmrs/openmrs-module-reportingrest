280a19d31269a0d0ec676d337fcf5b4d4024a2b0
djazayeri <jazayeri@alum.mit.edu>
2013-11-03 16:17:51 -0800

    Added placeholder implementation of DefinitionLibraryResource

---
+++ omod/src/main/java/org/openmrs/module/reportingrest/web/resource/DefinitionLibraryResource.java
@@ -0,0 +1,102 @@
+/*
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ */
+
+package org.openmrs.module.reportingrest.web.resource;
+
+import org.openmrs.api.context.Context;
+import org.openmrs.module.reporting.cohort.definition.CohortDefinition;
+import org.openmrs.module.reporting.data.encounter.definition.EncounterDataDefinition;
+import org.openmrs.module.reporting.data.patient.definition.PatientDataDefinition;
+import org.openmrs.module.reporting.definition.library.AllDefinitionLibraries;
+import org.openmrs.module.reporting.definition.library.LibraryDefinitionSummary;
+import org.openmrs.module.reporting.evaluation.Definition;
+import org.openmrs.module.reporting.query.encounter.definition.EncounterQuery;
+import org.openmrs.module.webservices.rest.SimpleObject;
+import org.openmrs.module.webservices.rest.web.ConversionUtil;
+import org.openmrs.module.webservices.rest.web.RequestContext;
+import org.openmrs.module.webservices.rest.web.RestConstants;
+import org.openmrs.module.webservices.rest.web.annotation.Resource;
+import org.openmrs.module.webservices.rest.web.representation.Representation;
+import org.openmrs.module.webservices.rest.web.resource.api.Retrievable;
+import org.openmrs.module.webservices.rest.web.resource.api.Searchable;
+import org.openmrs.module.webservices.rest.web.response.ObjectNotFoundException;
+import org.openmrs.module.webservices.rest.web.response.ResponseException;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ *
+ */
+@Resource(name = RestConstants.VERSION_1 + "/reportingrest/definitionLibrary", supportedClass = Definition.class, supportedOpenmrsVersions = {"1.8.*", "1.9.*"})
+public class DefinitionLibraryResource implements Searchable, Retrievable {
+
+    static Map<String, Class<? extends Definition>> types;
+    static {
+        types = new HashMap<String, Class<? extends Definition>>();
+        types.put("cohort", CohortDefinition.class);
+        types.put("encounterQuery", EncounterQuery.class);
+        types.put("patientData", PatientDataDefinition.class);
+        types.put("encounterData", EncounterDataDefinition.class);
+    }
+
+    @Override
+    public List<Representation> getAvailableRepresentations() {
+        return Arrays.asList(Representation.DEFAULT);
+    }
+
+    @Override
+    public SimpleObject search(RequestContext context) throws ResponseException {
+        Class<? extends Definition> definitionClass = getDefinitionClass(context, "q");
+        List<LibraryDefinitionSummary> summaries = getLibraries().getDefinitionSummaries(definitionClass);
+        SimpleObject results = new SimpleObject();
+        results.put("results", summaries);
+        return results;
+    }
+
+    private Class<? extends Definition> getDefinitionClass(RequestContext context, String paramName) {
+        String typeName = context.getParameter(paramName);
+        Class<? extends Definition> definitionClass = types.get(typeName);
+        if (definitionClass == null) {
+            try {
+                definitionClass = (Class<? extends Definition>) Context.loadClass(typeName);
+            } catch (ClassNotFoundException e) {
+                throw new IllegalArgumentException("Unknown type: " + typeName);
+            }
+        }
+        return definitionClass;
+    }
+
+    @Override
+    public Object retrieve(String uuid, RequestContext context) throws ResponseException {
+        Definition definition = getLibraries().getDefinition(null, uuid);
+        if (definition == null) {
+            throw new ObjectNotFoundException();
+        }
+        return ConversionUtil.convertToRepresentation(definition, context.getRepresentation());
+    }
+
+    @Override
+    public String getUri(Object instance) {
+        return RestConstants.URI_PREFIX + RestConstants.VERSION_1 +
+                "/reportingrest/definitionLibrary/" + ((Definition) instance).getUuid();
+    }
+
+    private AllDefinitionLibraries getLibraries() {
+        return Context.getRegisteredComponents(AllDefinitionLibraries.class).get(0);
+    }
+
+}
---
+++ omod/src/test/java/org/openmrs/module/reportingrest/web/resource/DefinitionLibraryResourceTest.java
@@ -0,0 +1,110 @@
+/*
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ */
+
+package org.openmrs.module.reportingrest.web.resource;
+
+import org.apache.commons.beanutils.PropertyUtils;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.hamcrest.BaseMatcher;
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.junit.Before;
+import org.junit.Test;
+import org.openmrs.api.context.Context;
+import org.openmrs.module.reporting.data.patient.definition.PatientIdDataDefinition;
+import org.openmrs.module.reporting.data.patient.library.BuiltInPatientDataLibrary;
+import org.openmrs.module.webservices.rest.SimpleObject;
+import org.openmrs.module.webservices.rest.web.RequestContext;
+import org.openmrs.module.webservices.rest.web.RestConstants;
+import org.openmrs.module.webservices.rest.web.api.RestService;
+import org.openmrs.web.test.BaseModuleWebContextSensitiveTest;
+import org.springframework.mock.web.MockHttpServletRequest;
+
+import java.util.List;
+
+import static org.hamcrest.core.IsNull.notNullValue;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+/**
+ *
+ */
+public class DefinitionLibraryResourceTest extends BaseModuleWebContextSensitiveTest {
+
+    private DefinitionLibraryResource resource;
+
+    @Before
+    public void setUp() throws Exception {
+        RestService restService = Context.getService(RestService.class);
+        resource = (DefinitionLibraryResource) restService.getResourceByName(RestConstants.VERSION_1 + "/reportingrest/definitionLibrary");
+    }
+
+    @Test
+    public void testListByType() throws Exception {
+        RequestContext context = buildRequestContext("q", "patientData");
+        SimpleObject result = resource.search(context);
+        assertThat(result.get("results"), notNullValue());
+
+        String json = new ObjectMapper().writeValueAsString(result);
+        assertTrue(json.contains(BuiltInPatientDataLibrary.PREFIX + "patientId"));
+
+        Matcher<?> elementMatcher = hasProperty("key", BuiltInPatientDataLibrary.PREFIX + "patientId");
+        boolean found = false;
+        for (Object item : (List<Object>) result.get("results")) {
+            if (elementMatcher.matches(item)) {
+                found = true;
+            }
+        }
+        assertTrue(found);
+    }
+
+    @Test
+    public void testRetrieveOne() throws Exception {
+        Object o = resource.retrieve(BuiltInPatientDataLibrary.PREFIX + "patientId", buildRequestContext());
+        assertTrue(o instanceof PatientIdDataDefinition);
+    }
+
+    private Matcher<?> hasProperty(final String name, final Object expectedValue) {
+        return new BaseMatcher<Object>() {
+            @Override
+            public boolean matches(Object o) {
+                Object actualValue;
+                try {
+                    actualValue = PropertyUtils.getProperty(o, name);
+                } catch (Exception e) {
+                    System.out.println("returning false");
+                    return false;
+                }
+                System.out.println("returning " + (actualValue == null ? (expectedValue == null) : actualValue.equals(expectedValue)));
+                return actualValue == null ? (expectedValue == null) : actualValue.equals(expectedValue);
+            }
+
+            @Override
+            public void describeTo(Description description) {
+                description.appendValue(name + " = " + expectedValue);
+            }
+        };
+    }
+
+    private RequestContext buildRequestContext(String... paramNamesAndValues) {
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        for (int i = 0; i < paramNamesAndValues.length; i += 2) {
+            request.addParameter(paramNamesAndValues[i], paramNamesAndValues[i + 1]);
+        }
+        RequestContext context = new RequestContext();
+        context.setRequest(request);
+        return context;
+    }
+
+}
